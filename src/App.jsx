5caa9c637973b42af555351beffc2239882e7309
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-02 10:50:47 +0100

    Basic Skeleton working

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,34 @@
 import { useState, useEffect } from "react";
 import Station from "./components/Station";
+
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
 import "./App.css";
 
 let textInput;
 
 function App() {
   const [stationList, setStationList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getRadioStations();
   }, []);
 
   async function getRadioStations() {
-    const res = await fetch(
-      "https://api.sr.se/api/v2/channels?format=json&size=100"
-    );
-    const data = await res.json();
-    /* console.log(data.channels); */
-    setStationList(data.channels);
+    try {
+      const res = await fetch(
+        "https://api.sr.se/api/v2/channels?format=json&size=100"
+      );
+      const data = await res.json();
+      /* console.log(data.channels); */
+      setStationList(data.channels);
+    } catch (e) {
+      console.error(e.error);
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   /* const filteredList = stationList.filter((st) =>{
@@ -35,22 +46,26 @@ function App() {
   return (
     <>
       <h1>RADIO STATIONS</h1>
-      <input type="text" onChange={readInput}></input>
-      <button onClick={readInput}> Search</button>
+      {/* <input type="text" onChange={readInput}></input>
+      <button onClick={readInput}> Search</button> */}
       <div>
-        <ol>
-          {stationList.map((st) => (
-            <li key={st.id}>
-              <div className="stationContainer">
-                <Station
-                  name={st.name}
-                  source={st.liveaudio.url}
-                  logoImg={st.imagetemplate}
-                />
-              </div>
-            </li>
-          ))}
-        </ol>
+        {isLoading ? (
+          <Skeleton count={10} />
+        ) : (
+          <ol>
+            {stationList.map((st) => (
+              <li key={st.id}>
+                <div className="stationContainer">
+                  <Station
+                    source={st.liveaudio.url}
+                    logoImg={st.imagetemplate}
+                    name={st.name}
+                  />
+                </div>
+              </li>
+            ))}
+          </ol>
+        )}
       </div>
     </>
   );
